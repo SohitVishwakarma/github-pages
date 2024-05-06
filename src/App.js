6a9c8d988b9ab66646781d853b7de419b78db990
akash <akash@arivani.net>
2024-05-06 13:29:24 +0530

    changes2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        TEST CI/CD rtgb rtbrt bgrw
+        TEST CI/CD 
         </a>
       </header>
     </div>
